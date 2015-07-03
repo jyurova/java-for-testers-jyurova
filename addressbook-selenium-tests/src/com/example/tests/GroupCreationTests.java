cde9b83c2f0a545e2b7dae8cfa0d8bbf9603dffd
jyurova <jyurova@intermedia.net>
2015-07-03 17:24:16 +0300

    TestNG

--- addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -3,12 +3,16 @@ package com.example.tests;
 
 import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
-import org.junit.*;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class GroupCreationTests {
   private WebDriver driver;
@@ -16,7 +20,7 @@ public class GroupCreationTests {
   private boolean acceptNextAlert = true;
   private StringBuffer verificationErrors = new StringBuffer();
 
-  @Before
+  @BeforeClass
   public void setUp() throws Exception {
     driver = new FirefoxDriver();
     baseUrl = "http://localhost/";
@@ -75,7 +79,7 @@ private void openMainPage() {
     driver.get(baseUrl + "/addressbookv4.1.4/");
 }
 
-  @After
+  @AfterClass
   public void tearDown() throws Exception {
     driver.quit();
     String verificationErrorString = verificationErrors.toString();
