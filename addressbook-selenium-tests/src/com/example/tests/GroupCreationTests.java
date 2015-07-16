8877c105e1d3ff6b172ed33523afc09c2d8ff3bc
jyurova <jyurova@intermedia.net>
2015-07-16 22:14:25 +0300

    class work

--- addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupCreationTests.java
@@ -1,30 +1,30 @@
 package com.example.tests;
 
+import java.util.Collections;
+import java.util.List;
+import static org.testng.Assert.assertEquals;
 import org.testng.annotations.Test;
 
 public class GroupCreationTests extends TestBase {
-	
-  @Test
-  public void testNonEmptyGroupCreation() throws Exception {
+		
+  @Test(dataProvider = "randomValidGroupGenerator")
+  public void testGroupCreationWithValidData(GroupData group) throws Exception {
 	app.getNavigationHelper().openMainPage();
 	app.getGroupHelper().goToGroupsPage();
+	//save old state
+	List<GroupData> oldList = app.getGroupHelper().getGroups();
+	
+	//actions
     app.getGroupHelper().initNewGroupCreation();
-    GroupData group = new GroupData();
-    group.name = "group name 1";
-    group.header = "header 1";
-    group.footer = "footer 1";    	
 	app.getGroupHelper().fillGroupForm(group);
     app.getGroupHelper().submitGroupCreation();
     app.getNavigationHelper().returnToGroupsPage();
+    
+    //save new states
+    List<GroupData> newList = app.getGroupHelper().getGroups();
+    // compare states 
+    oldList.add(group);
+    Collections.sort(oldList);
+    assertEquals(newList, oldList);
   }
-
-  @Test
-  public void testEmptyGroupCreation() throws Exception {
-	app.getNavigationHelper().openMainPage();
-    app.getGroupHelper().goToGroupsPage();
-    app.getGroupHelper().initNewGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData(" ", " ", " "));
-    app.getGroupHelper().submitGroupCreation();
-    app.getNavigationHelper().returnToGroupsPage();
-  }
-}
+} 
--- addressbook-selenium-tests/src/com/example/tests/GroupRemovalTests.java
+++ addressbook-selenium-tests/src/com/example/tests/GroupRemovalTests.java
@@ -1,5 +1,11 @@
 package com.example.tests;
 
+import static org.testng.Assert.assertEquals;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 import org.testng.annotations.Test;
 
 public class GroupRemovalTests extends TestBase {
@@ -7,8 +13,23 @@ public class GroupRemovalTests extends TestBase {
 	public void deleteSomeGroup() {
 		app.getNavigationHelper().openMainPage();
 		app.getGroupHelper().goToGroupsPage();
-		app.getGroupHelper().deleteGroup(1);
+		
+		//save old state
+		List<GroupData> oldList = app.getGroupHelper().getGroups();
+		
+		Random rnd = new Random();
+		int index = rnd.nextInt(oldList.size()-1);
+
+		app.getGroupHelper().deleteGroup(index);
 	    app.getNavigationHelper().returnToGroupsPage();
+	    
+	    //save new states
+	    List<GroupData> newList = app.getGroupHelper().getGroups();
+	    
+	    // compare states 
+	    oldList.remove(index);
+	    Collections.sort(oldList);
+	    assertEquals(newList, oldList);
 	}
 
 }
