02ba75443c5f5d2be22883fd8ce62c9221064d27
jyurova <jyurova@intermedia.net>
2015-08-29 17:22:47 +0300

    Revert "HT 11"
    This reverts commit ea7ae71728898fb518aad94be85f796d4e9f60b9.

--- addressbook-selenium-tests/src/com/example/tests/TestBase.java
+++ addressbook-selenium-tests/src/com/example/tests/TestBase.java
@@ -3,16 +3,17 @@ package com.example.tests;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Random;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
-import static com.example.tests.GroupDataGenerator.generateRandomGroups;
-import static com.example.tests.ContactDataGenerator.generateRandomContacts;
+
 import com.example.fw.ApplicationManager;
 
 public class TestBase {
 	
 	protected ApplicationManager app;
+
 
 	@BeforeTest
 	public void setUp() throws Exception {
@@ -28,27 +29,48 @@ public class TestBase {
 	@DataProvider
 	
 	public Iterator<Object[]> randomValidGroupGenerator() {
-		return wrapGroupDataForDataProvider(generateRandomGroups(5)).iterator();
+		List<Object[]> list = new ArrayList<Object[]>();
+		
+		for (int i = 0; i < 5; i++) {
+			GroupData group = new GroupData()
+					.withName(generateRandomString())
+					.withHeader(generateRandomString()) 
+					.withFooter(generateRandomString());
+			list.add(new Object[]{group});
+		}
+		return list.iterator();
 	}
 	
-		private List<Object[]> wrapGroupDataForDataProvider(List<GroupData> groups) {
-		List<Object[]> list = new ArrayList<Object[]>();
-		for (GroupData group: groups) {
-			list.add(new Object[] {group});
-		}
-		return list;
-	}
-
 		@DataProvider
 		public Iterator<Object[]> randomValidContactGenerator() {
-			return wrapContactsForDataProvider(generateRandomContacts(5)).iterator();
-		}
-
-		private List<Object[]> wrapContactsForDataProvider(List<ContactData> contacts) {
 			List<Object[]> list = new ArrayList<Object[]>();
-			for (ContactData contact : contacts) {
+			for (int i = 0; i < 5; i++){
+				ContactData contact = new ContactData()
+					.withFirstname (generateRandomString()) 
+					.withLastname  (generateRandomString())
+					.withAddress  (generateRandomString())
+					.withHome  (generateRandomString())
+					.withMobile  (generateRandomString())
+					.withWork  (generateRandomString())
+					.withEmail  (generateRandomString())
+					.withEmail2  (generateRandomString())
+					//.withByear  (generateRandomString())
+					.withPhone2  (generateRandomString())
+					.withAddress2  (generateRandomString())
+					//.withBday  (generateRandomString())
+					.withBmonth  (generateRandomString()); 
+				contact.title = contact.getFirstname() + " " + contact.getLastname();
 				list.add(new Object[]{contact});
 			}
-			return list;
+			return list.iterator();
+		}
+
+		public String generateRandomString() {
+			Random rnd = new Random();
+			if (rnd.nextInt(3) == 0 ) {
+				return "1";
+			}else{
+				return "test" + rnd.nextInt();
+			}
 		}
 }
